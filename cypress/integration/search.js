f93ecc79fc2f5ca001991eb862b0cb5fad6d2884
Umi <umy.ikem@gmail.com>
2021-04-13 19:01:37 +0100

    enter search term & click result

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -4,8 +4,16 @@ describe("Search Validations", () => {
 
     it("Validate URL after search", () => {
         
-       
-
+        cy.visit("https://www.bitfinex.com/")
+        cy.get('.bp3-tab-list a.landing-tickers__search', { timeout: 2000 })
+        .click()
+        cy.get('div.visible-desktop .bp3-input-group.landing-tickers__search-group > input', { timeout: 4000 })
+        .type('Unus Sed Leo')
+        cy.get('.symbol-col', { timeout: 5000 })
+        .click()
+        
+        
+        
 
     })
 })
