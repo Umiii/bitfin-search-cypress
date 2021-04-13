002ed079337564caa30ca5b0372a87b6096daa77
Umi <umy.ikem@gmail.com>
2021-04-13 19:05:51 +0100

    add url assertion

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -4,14 +4,14 @@ describe("Search Validations", () => {
 
     it("Validate URL after search", () => {
         
-        cy.visit("https://www.bitfinex.com/")
+        cy.visit("https://www.bitfinex.com/");
         cy.get('.bp3-tab-list a.landing-tickers__search', { timeout: 2000 })
-        .click()
+        .click();
         cy.get('div.visible-desktop .bp3-input-group.landing-tickers__search-group > input', { timeout: 4000 })
-        .type('Unus Sed Leo')
+        .type('Unus Sed Leo');
         cy.get('.symbol-col', { timeout: 5000 })
-        .click()
-        
+        .click();
+        cy.url().should('eq', 'https://trading.bitfinex.com/t/LEO:USD?demo=true');
         
         
 
